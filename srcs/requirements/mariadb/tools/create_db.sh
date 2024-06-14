2bd75e7322f6731aa4045e2dcbc8869c96b91884
edvin3i <edviniii@gmail.com>
2024-06-14 15:28:18 +0200

    added conf for mariadb

---
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -0,0 +1,18 @@
+#!bin/sh
+cat << EOF > /tmp/create_db.sql                               # создание базы
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM     mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
+CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
+GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+/usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql  # выполняем код 
+
+rm -f /tmp/create_db.sql
