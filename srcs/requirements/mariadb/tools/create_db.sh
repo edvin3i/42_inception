decb96e0388e2ebefe1b2ede435ae632529701f7
edvin3i <edviniii@gmail.com>
2024-06-17 20:21:58 +0200

    fixing wp db creation

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -9,8 +9,8 @@ DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
 CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
--- srcs/requirements/wordpress/tools/wp_install.sh
+++ srcs/requirements/wordpress/tools/wp_install.sh
@@ -4,17 +4,26 @@ wp core download --path=/var/www \
                  --locale=en_US
 
 wp config create --dbname=${DB_NAME} \
-                 --dbhost='mariadb:3306' \
+                 --dbhost=mariadb \
                  --dbuser=${DB_USER} \
-                 --prompt=${DB_PASSWORD} \
+                 --dbpass=${DB_PASSWORD} \
                  --dbprefix='wp_' \
                  --dbcharset='utf8' \
                  --dbcollate=''
 
 wp db create
 
-wp core install --url=${DOMAIN_NAME} \
-                --title=${WP_TITLE} \
-                --admin_user=${WP_ADMIN} \
-                --admin_password=${WP_ADM_PASS} \
-                --admin_email=${WP_ADM_EMAIL}
+# wp core install --url=${DOMAIN_NAME} \
+#                 --title=${WP_TITLE} \
+#                 --admin_user=${WP_ADMIN} \
+#                 --admin_password=${WP_ADM_PASS} \
+#                 --admin_email=${WP_ADM_EMAIL}
+
+
+# wp config create --dbname=wordpress \
+#                  --dbhost=mariadb \
+#                  --dbuser=martien \
+#                  --dbpass=r3DplAne5t \
+#                  --dbprefix='wp_' \
+#                  --dbcharset='utf8' \
+#                  --dbcollate=''
